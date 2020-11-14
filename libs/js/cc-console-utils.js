ce74f9ff1a3caa1f5b64b3c18a98160df32c327a
yanjifa <yanjifa@joycastle.mobi>
2020-11-14 23:32:03 +0800

    :art: for game-template

--- libs/js/cc-console-utils.js
+++ libs/js/cc-console-utils.js
@@ -4,14 +4,14 @@ const initConsoleUtil = function () {
         let index = key || 0;
         let treeNode = function (node) {
             let nameStyle =
-                `color: ${node.parent === null || node.activeInHierarchy ? 'green' : 'grey'}; font-size: 14px;font-weight:bold`;
+                `color: ${node.parent === null || node.activeInHierarchy ? "green" : "grey"}; font-size: 14px;font-weight:bold`;
             let propStyle =
-                `color: black; background: lightgrey;margin-left: 5px;border-radius:3px;padding: 0 3px;font-size: 10px;font-weight:bold`;
+                "color: black; background: lightgrey;margin-left: 5px;border-radius:3px;padding: 0 3px;font-size: 10px;font-weight:bold";
             let indexStyle =
-                `color: orange; background: black;margin-left: 5px;border-radius:3px;padding:0 3px;fonrt-size: 10px;font-weight:bold;`
+                "color: orange; background: black;margin-left: 5px;border-radius:3px;padding:0 3px;fonrt-size: 10px;font-weight:bold;";
             let nameValue = `%c${node.name}`;
             let propValue =
-                `%c${node.x.toFixed(0) + ',' + node.y.toFixed(0) + ',' + node.width.toFixed(0) + ',' + node.height.toFixed(0) + ',' + node.scale.toFixed(1)}`
+                `%c${node.x.toFixed(0) + "," + node.y.toFixed(0) + "," + node.width.toFixed(0) + "," + node.height.toFixed(0) + "," + node.scale.toFixed(1)}`;
             let indexValue = `%c${index++}`;
             if (node.childrenCount > 0) {
                 console.groupCollapsed(nameValue + propValue + indexValue, nameStyle,
@@ -24,17 +24,17 @@ const initConsoleUtil = function () {
                 console.log(nameValue + propValue + indexValue, nameStyle, propStyle,
                     indexStyle);
             }
-        }
+        };
         if (key) {
             let node = cc.cat(key);
-            index = node['tempIndex'];
+            index = node["tempIndex"];
             treeNode(node);
         } else {
             let scene = cc.director.getScene();
             treeNode(scene);
         }
-        return '属性依次为x,y,width,height,scale.使用cc.cat(id)查看详细属性.';
-    }
+        return "属性依次为x,y,width,height,scale.使用cc.cat(id)查看详细属性.";
+    };
     cc.cat = function (key) {
         let index = 0;
         let target;
@@ -58,12 +58,12 @@ const initConsoleUtil = function () {
                     sortId(node.children[i]);
                 }
             }
-        }
+        };
         let scene = cc.director.getScene();
         sortId(scene);
-        target['tempIndex'] = cc.js.isNumber(key) ? key : index;
+        target["tempIndex"] = cc.js.isNumber(key) ? key : index;
         return target;
-    }
+    };
     cc.list = function (key) {
         let targets = [];
         let step = function (node) {
@@ -75,7 +75,7 @@ const initConsoleUtil = function () {
                     step(node.children[i]);
                 }
             }
-        }
+        };
         let scene = cc.director.getScene();
         step(scene);
         if (targets.length === 1) {
@@ -83,7 +83,7 @@ const initConsoleUtil = function () {
         } else {
             return targets;
         }
-    }
+    };
     cc.where = function (key) {
         let target = key.name ? key : cc.cat(key);
         if (!target) {
@@ -106,7 +106,7 @@ const initConsoleUtil = function () {
             bgNode.width = rect.width;
             bgNode.height = rect.height;
             graphics.rect(-bgNode.width / 2, -bgNode.height / 2, bgNode.width, bgNode.height);
-            graphics.fillColor = new cc.Color().fromHEX('#E91E6390');
+            graphics.fillColor = new cc.Color().fromHEX("#E91E6390");
             graphics.fill();
         }
         setTimeout(() => {
@@ -115,20 +115,20 @@ const initConsoleUtil = function () {
             }
         }, 2000);
         return target;
-    }
+    };
     cc.cache = function () {
         let rawCacheData = cc.assetManager.assets._map;
         let cacheData = [];
         let totalTextureSize = 0;
         for (let k in rawCacheData) {
             let item = rawCacheData[k];
-            if (item.type !== 'js' && item.type !== 'json') {
-                let itemName = '_';
-                let preview = '';
+            if (item.type !== "js" && item.type !== "json") {
+                let itemName = "_";
+                let preview = "";
                 let content = item.__classname__;
                 let formatSize = -1;
-                if (item.type === 'png' || item.type === 'jpg') {
-                    let texture = rawCacheData[k.replace('.' + item.type, '.json')];
+                if (item.type === "png" || item.type === "jpg") {
+                    let texture = rawCacheData[k.replace("." + item.type, ".json")];
                     if (texture && texture._owner && texture._owner._name) {
                         itemName = texture._owner._name;
                         preview = texture.content.url;
@@ -137,15 +137,15 @@ const initConsoleUtil = function () {
                     if (item.name) {
                         itemName = item.name;
                     } else if (item._owner) {
-                        itemName = (item._owner && item._owner.name) || '_';
+                        itemName = (item._owner && item._owner.name) || "_";
                     }
-                    if (content === 'cc.Texture2D') {
+                    if (content === "cc.Texture2D") {
                         preview = item.nativeUrl;
-                        let textureSize = item.width * item.height * ((item._native === '.jpg' ? 3 : 4) / 1024 / 1024);
+                        let textureSize = item.width * item.height * ((item._native === ".jpg" ? 3 : 4) / 1024 / 1024);
                         totalTextureSize += textureSize;
                         // sizeStr = textureSize.toFixed(3) + 'M';
                         formatSize = Math.round(textureSize * 1000) / 1000;
-                    } else if (content === 'cc.SpriteFrame') {
+                    } else if (content === "cc.SpriteFrame") {
                         preview = item._texture.nativeUrl;
                     }
                 }
@@ -155,11 +155,11 @@ const initConsoleUtil = function () {
                     name: itemName,
                     preview: preview,
                     id: item._uuid,
-                    size: formatSize
+                    size: formatSize,
                 });
             }
         }
-        let cacheTitle = `缓存 [文件总数:${cacheData.length}][纹理缓存:${totalTextureSize.toFixed(2) + 'M'}]`;
+        let cacheTitle = `缓存 [文件总数:${cacheData.length}][纹理缓存:${totalTextureSize.toFixed(2) + "M"}]`;
         return [cacheData, cacheTitle];
-    }
-}
+    };
+};
--- preview.js
+++ preview.js
@@ -1,19 +1,19 @@
 const app = new Vue({
-    el: '#app',
+    el: "#app",
     vuetify: new Vuetify({
-        theme: { dark: true }
+        theme: { dark: true },
     }),
     data: {
         isShowTop: true,
         drawer: false,
         cacheDialog: false,
-        cacheTitle: '',
+        cacheTitle: "",
         cacheHeaders: [
-            { text: 'Type', value: 'type' },
-            { text: 'Name', value: 'name' },
-            { text: 'Preivew', value: 'preview' },
-            { text: 'ID', value: 'id' },
-            { text: 'Size', value: 'size' },
+            { text: "Type", value: "type" },
+            { text: "Name", value: "name" },
+            { text: "Preivew", value: "preview" },
+            { text: "ID", value: "id" },
+            { text: "Size", value: "size" },
         ],
         cacheRawData: [],
         cacheData: [],
@@ -27,7 +27,8 @@ const app = new Vue({
         componentsSchema: [],
     },
     created() {
-        if (window.innerHeight === window.outerHeight) { // 手机端，chrome device模式
+        // 手机端，chrome device模式
+        if (window.innerHeight === window.outerHeight) {
             this.isShowTop = false;
         }
         this.waitCCInit().then(() => {
@@ -40,19 +41,19 @@ const app = new Vue({
     watch: {
         cacheOnlyTexture() {
             this.updateCacheData();
-        }
+        },
     },
     computed: {
         treeFilter() {
             return (item, search, textKey) => item[textKey].indexOf(search) > -1;
         },
         selectedNode() {
-            if (!this.selectedNodes.length) return undefined
+            if (!this.selectedNodes.length) return undefined;
             let node = getNodeById(this.selectedNodes[0]);
             if (node) {
                 if (!node.hex_color) {
-                    cc.js.getset(node, 'hex_color', () => {
-                        return '#' + node.color.toHEX('#rrggbb');
+                    cc.js.getset(node, "hex_color", () => {
+                        return "#" + node.color.toHEX("#rrggbb");
                     }, (hex) => {
                         node.color = new cc.Color().fromHEX(hex);
                     }, false, true);
@@ -64,7 +65,7 @@ const app = new Vue({
                     if (this.selectedNodes.length > 0 && this.selectedNodes[0] === node._id) {
                         this.selectedNodes.pop();
                     }
-                }
+                };
                 this.nodeSchema = NEX_CONFIG.nodeSchema.node2d;
                 let componentsSchema = [];
                 for (let component of node._components) {
@@ -72,10 +73,10 @@ const app = new Vue({
                     if (schema) {
                         node[schema.key] = node.getComponent(schema.key);
                         for (let i = 0; i < schema.rows.length; i++) {
-                            if (schema.rows[i].type === 'color') {
+                            if (schema.rows[i].type === "color") {
                                 if (!node[schema.key][schema.rows[i].key]) {
                                     cc.js.getset(node[schema.key], schema.rows[i].key, () => {
-                                        return '#' + node.getComponent(schema.key)[schema.rows[i].rawKey].toHEX('#rrggbb');
+                                        return "#" + node.getComponent(schema.key)[schema.rows[i].rawKey].toHEX("#rrggbb");
                                     }, (hex) => {
                                         node.getComponent(schema.key)[schema.rows[i].rawKey] = new cc.Color().fromHEX(hex);
                                     }, false, true);
@@ -85,7 +86,7 @@ const app = new Vue({
                     } else {
                         schema = {
                             title: component.__classname__,
-                            key: component.__classname__
+                            key: component.__classname__,
                         };
                         node[schema.key] = node.getComponent(schema.key);
                     }
@@ -121,28 +122,28 @@ const app = new Vue({
         },
         outputNodeHandler(id) {
             let i = 1;
-            while (window['temp' + i] !== undefined) {
+            while (window["temp" + i] !== undefined) {
                 i++;
             }
-            window['temp' + i] = this.selectedNode;
-            console.log('temp' + i);
-            console.log(window['temp' + i]);
+            window["temp" + i] = this.selectedNode;
+            console.info("temp" + i);
+            console.info(window["temp" + i]);
         },
         outputComponentHandler(component) {
             let i = 1;
-            while (window['temp' + i] !== undefined) {
+            while (window["temp" + i] !== undefined) {
                 i++;
             }
-            window['temp' + i] = this.selectedNode.getComponent(component);
-            console.log('temp' + i);
-            console.log(window['temp' + i]);
+            window["temp" + i] = this.selectedNode.getComponent(component);
+            console.info("temp" + i);
+            console.info(window["temp" + i]);
         },
         drawNodeRect() {
             cc.where(this.selectedNode);
         },
         updateCacheData() {
             if (this.$data.cacheOnlyTexture) {
-                this.$data.cacheData = this.$data.cacheRawData.filter(item => item.type === 'cc.Texture2D');
+                this.$data.cacheData = this.$data.cacheRawData.filter(item => item.type === "cc.Texture2D");
             } else {
                 this.$data.cacheData = this.$data.cacheRawData;
             }
@@ -153,15 +154,15 @@ const app = new Vue({
             this.$data.cacheDialog = true;
         },
         openGithub() {
-            window.open('https://github.com/potato47/ccc-devtools');
+            window.open("https://github.com/potato47/ccc-devtools");
         },
         openCocosForum() {
-            window.open('https://forum.cocos.com/');
+            window.open("https://forum.cocos.com/");
         },
         openCocosDocs() {
-            window.open('https://docs.cocos.com/');
-        }
-    }
+            window.open("https://docs.cocos.com/");
+        },
+    },
 });
 
 function getChildren(node) {
@@ -185,7 +186,7 @@ function getNodeById(id) {
                 }
             }
         }
-    }
+    };
     const scene = cc.director.getScene();
     search(scene);
     return target;
